7dd4fa3b04de453ed8a445c859f6c22a5f8d09d2
martin (darkmtbg2) <martin-baage@hotmail.com>
2020-08-04 10:10:28 +0200

    Added Abstact base class

---
+++ CocktailEntityFramework/classes/IngredientBase.cs
@@ -0,0 +1,19 @@
+﻿using CocktailEntityFramework.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailEntityFramework.classes
+{
+    abstract class IngredientBase : IIngredient
+    {
+        public string Name { get; private set; }
+
+        protected IngredientBase(string name)
+        {
+            Name = name;
+        }
+    }
+}
