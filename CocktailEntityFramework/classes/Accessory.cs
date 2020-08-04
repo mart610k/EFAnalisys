084920b4951bf5af035f74609179bf64fc5ea6b1
martin (darkmtbg2) <martin-baage@hotmail.com>
2020-08-04 10:10:41 +0200

    Added Accessory

---
+++ CocktailEntityFramework/classes/Accessory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailEntityFramework.classes
+{
+    class Accessory : IngredientBase
+    {
+        public Accessory(string name) : base(name)
+        {
+
+        }
+    }
+}
