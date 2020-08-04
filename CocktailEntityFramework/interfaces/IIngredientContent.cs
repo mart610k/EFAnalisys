790c39092f93edd04dbc83827eb0929612332ebf
martin (darkmtbg2) <martin-baage@hotmail.com>
2020-08-04 09:55:02 +0200

    Added Incredient Contents

---
+++ CocktailEntityFramework/interfaces/IIngredientContent.cs
@@ -0,0 +1,18 @@
+﻿using CocktailEntityFramework.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailEntityFramework.interfaces
+{
+    interface IIngredientContent
+    {
+        IIngredient Ingredient { get; }
+        
+        UnitType UnitType { get; }
+
+        int UnitAmount { get; }
+    }
+}
