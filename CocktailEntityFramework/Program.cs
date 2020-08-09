81e03ffebf3aa66f1effca202eb79c5ea42940e1
mart610k <martin-baage@hotmail.com>
2020-08-09 19:20:11 +0200

    Init incredient and drinks

--- CocktailEntityFramework/Program.cs
+++ CocktailEntityFramework/Program.cs
@@ -1,4 +1,4 @@
-﻿using CocktailEntityFramework.classes;
+using CocktailEntityFramework.classes;
 using System;
 using System.Collections.Generic;
 using CocktailEntityFramework.interfaces;
@@ -36,24 +36,56 @@ namespace CocktailEntityFramework
             Console.ReadKey();
         }
 
-       static void AddDatabaseEntries()
+        static void AddIngredients()
        {
             using (IngredientContext ctx = new IngredientContext())
             {
+                
                 ctx.AddIngridientBase(new Liqour("Vodka", 37.5f));
                 ctx.AddIngridientBase(new Liqour("Whisky", 40f));
                 ctx.AddIngridientBase(new Mixer("Apple Juice"));
                 ctx.AddIngridientBase(new Mixer("Mango Juice"));
                 ctx.AddIngridientBase(new Mixer("Orange Juice"));
-                ctx.AddIngridientBase(new Accessory("Ice cube"));
+                ctx.AddIngridientBase(new Mixer("Grapefruit Juice"));
                 ctx.AddIngridientBase(new Accessory("Straw"));
                 ctx.AddIngridientBase(new Accessory("Melon"));
                 ctx.AddIngridientBase(new Accessory("Olive"));
                 ctx.AddIngridientBase(new Liqour("Gin", 40f));
-
+                ctx.AddIngridientBase(new Mixer("Cranberry Juice"));
                 ctx.AddIngridientBase(new Mixer("Sprite"));
                 ctx.AddIngridientBase(new Mixer("Lime Juice"));
                 ctx.AddIngridientBase(new Mixer("Cranberry Juice"));
+                ctx.AddIngridientBase(new Accessory("Ice"));
+                ctx.AddIngridientBase(new Accessory("Lime"));
+
+
+            }
+        }
+        static void AddDrinks()
+        {
+            using (IngredientContext ctx = new IngredientContext())
+            {
+
+                IngredientBase vodka = ctx.IngredientBase.Where(s => s.Name == "Vodka").FirstOrDefault();
+                IngredientBase cranberryJuice = ctx.IngredientBase.Where(s => s.Name == "Cranberry Juice").FirstOrDefault();
+                IngredientBase grapefruitJuice = ctx.IngredientBase.Where(s => s.Name == "Grapefruit Juice").FirstOrDefault();
+                IngredientBase ice = ctx.IngredientBase.Where(s => s.Name == "Ice").FirstOrDefault();
+                IngredientBase lime = ctx.IngredientBase.Where(s => s.Name == "Lime").FirstOrDefault();
+
+                Recipe seaBreeze = new Recipe("Sea Breeze");
+
+                seaBreeze.AddIngredientContent(new IngredientContent(grapefruitJuice, enums.UnitType.ml, 50));
+                seaBreeze.AddIngredientContent(new IngredientContent(cranberryJuice, enums.UnitType.ml, 120));
+                seaBreeze.AddIngredientContent(new IngredientContent(vodka, enums.UnitType.ml, 40));
+                seaBreeze.AddIngredientContent(new IngredientContent(ice, enums.UnitType.piece, 1));
+                seaBreeze.AddIngredientContent(new IngredientContent(lime, enums.UnitType.slice, 1));
+
+                Recipe vodkaDrink = new Recipe("VODKA");
+                vodkaDrink.AddIngredientContent(new IngredientContent(vodka, enums.UnitType.ml, 100));
+
+                ctx.Recipe.Add(seaBreeze);
+                ctx.Recipe.Add(vodkaDrink);
+                ctx.SaveChanges();
             }
        }
     }
